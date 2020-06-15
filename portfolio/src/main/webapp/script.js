8c0a8dcaa9269b6bc5f3ee2c023b9436e06c3ad2
Anh Huynh <anhhuynh.cs@gmail.com>
2020-06-15 21:46:13 +0700

    Add stash

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,17 +17,17 @@
  */
 function addRandomGreeting() {
   const greetings = [
-    'Hello world!',
-    '¡Hola Mundo!',
-    '你好，世界！',
-    'Bonjour le monde!'
+    "Hello world!",
+    "¡Hola Mundo!",
+    "你好，世界！",
+    "Bonjour le monde!",
   ];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
 
   // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
+  const greetingContainer = document.getElementById("greeting-container");
   greetingContainer.innerText = greeting;
 }
 
@@ -35,10 +35,12 @@ function addRandomGreeting() {
  * Adds comments from /data to the page.
  */
 function getComments() {
-  fetch('/data')
-    .then(response => response.json())
-    .then(comments => {
-      document.getElementById('notes-container').innerHTML = `${comments.map(comment => `<p>${comment}</p>`).join("")}`;
+  fetch("/data")
+    .then((response) => response.json())
+    .then((comments) => {
+      document.getElementById("notes-container").innerHTML = `${comments
+        .map((comment) => `<p>${comment.comment}</p>`)
+        .join("")}`;
     });
 }
 
